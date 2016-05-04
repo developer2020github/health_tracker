95ff0b55777f1be155e30ceda358bcdab8bb9905
developer2020 <dev276236@gmail.com>
2016-05-04 09:10:22 +0400

    Got total calroies working (prototype)

--- src/js/selected_list_of_food_items_view.js
+++ src/js/selected_list_of_food_items_view.js
@@ -1,28 +1,26 @@
 var SelectedListOfFoodItems = Backbone.View.extend({
-       
-	el: '#selected-list',
 
-	total_template: _.template($('#total-calories-template').html()),
-    initialize: function () {
-    	this.$list=$('#selected-list');
-    	this.$total_calories_el=$('#total-calories-selected');
+    el: '#selected-list',
+
+    total_template: _.template($('#total-calories-template').html()),
+    initialize: function() {
+        this.$list = $('#selected-list');
+        this.$total_calories_el = $('#total-calories-selected');
+        this.total_calories = 0;
     },
 
-    total_calories: 0,
+    render: function() {
 
-    render: function (){
-      console.log("render");
-      var htmlOutput = this.total_template({calories: this.total_calories});
-      console.log(htmlOutput);
-      this.$total_calories_el.append(htmlOutput);
-      return this;
+        var htmlOutput = this.total_template({ calories: this.total_calories });
+        this.$total_calories_el.html(htmlOutput);
+        return this;
     },
 
-	add_item: function(food_item){
-	  var food_item_view = new FoodItemView({ model: food_item });
-	  this.$list.append(food_item_view.render().el);
-	  this.total_calories  = this.total_calories + food_item.calories; 
-	  this.render(); 
-	 }
+    add_item: function(food_item) {
+        var food_item_view = new FoodItemView({ model: food_item });
+        this.$list.append(food_item_view.render().el);
+        this.total_calories = this.total_calories + food_item.get("calories");
+        this.render();
+    }
 
-	});
+});
